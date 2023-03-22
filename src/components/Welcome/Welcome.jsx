2486f6728bfbe3f68bd355a516ef98fbd833af5f
Cauabc <cauacardosoyt@gmail.com>
2023-03-22 14:36:36 -0300

    Sign in and sign up buttons added

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -18,7 +18,25 @@ const Container = styled.div`
     & p {
         margin-top: 1rem;
     }
+`
 
+const SignForm = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    margin-top: 3rem;
+`
+
+const Button = styled.button`
+    background-color: ${({backgroundColor}) => backgroundColor};
+    border: 0;
+    width: 150px;
+    height: 60px;
+    color: ${({color}) => color};
+    border-radius: 25px;
+    font-weight: bold;
+    cursor: pointer;
+    margin-right: 1rem;
 `
 
 export default function Welcome() {
@@ -26,6 +44,14 @@ export default function Welcome() {
     <Container>
         <h1>Welcome</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat saepe cupiditate blanditiis sit inventore maxime officia iure necessitatibus debitis.</p>
+        <SignForm>
+            <Button backgroundColor="#000" color='#fff'>
+                Sign In
+            </Button>
+            <Button backgroundColor="#fff" color='#000'>
+                Sign Up
+            </Button>
+        </SignForm>
     </Container>
   )
 }
