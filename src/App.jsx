ce6ffee004382bb02abfad4414b7b5536d174ed1
Cauabc <cauacardosoyt@gmail.com>
2023-04-23 18:58:09 -0300

    Bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,58 @@
 import SignIn from "./pages/SignIn/SignIn"
 import SignUp from "./pages/SignUp/SignUp"
 import Content from "./components/Content/Content"
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route, useLocation} from 'react-router-dom'
+import { createGlobalStyle } from "styled-components";
+import { useEffect } from "react";
+
+
+export const GlobalStyles = createGlobalStyle`
+    *{
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+    
+    body{
+      min-height: 100vh;
+      overflow-y: ${(props) => props.overflow};
+      overflow-x: hidden;
+      font-family: 'Ubuntu', sans-serif;
+    }`
 
 
 function App() {
-  
+
+    const location = useLocation();
+
+    useEffect(() => {
+      const handleRouteChange = () => {
+        const body = document.querySelector("body");
+        if (location.pathname === "/") {
+          body.style.overflow = "hidden";
+        } else {
+          body.style.overflow = "auto";
+        }
+      };
+
+      handleRouteChange();
+      window.addEventListener("popstate", handleRouteChange);
+      return () => {
+        window.removeEventListener("popstate", handleRouteChange);
+      };
+    }, [location]);
+    
 
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Content/>}/>
-        <Route path="/signin" element={<SignIn/>}/>
-        <Route path="/signup" element={<SignUp/>}/>
-      </Routes>
+      <GlobalStyles overflow="auto"/>
+        <Routes>
+          <Route path="/" element={<Content/>}/>
+          <Route path="/signin" element={<SignIn/>}/>
+          <Route path="/signup" element={<SignUp/>}/>
+        </Routes>
     </>
+
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { GlobalStyles } from './GlobalStyles'
+import { GlobalStyles } from './App'
 import {BrowserRouter } from 'react-router-dom'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
